1c2477ab9acb27301b1053d4e4a2fca2faa540a2
Igor Nemykin <nemykin.i.o@sberbank.ru>
2021-08-28 07:00:04 +0300

    Задание 11: Создать новый React с использованием react UI library [Ant Design of React](https://ant.design/docs/react/getting-started) проект и изменить текст на странице как вам понравилось первое занятие? И что Вы хотели бы улучшить.

--- src/App.js
+++ src/App.js
@@ -5,18 +5,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo"/>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Первый урок прошел быстро, информативно, но немного сумбурно
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          Хочется побольше творческой практики, например выбор темы для создания своего приложения и вести его весь курс
+        </p>
       </header>
     </div>
   );
