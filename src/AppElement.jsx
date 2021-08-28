f8aad78a5ab2c4fa94134bfb8d6779ef0e60bd5d
Igor Nemykin <nemykin.i.o@sberbank.ru>
2021-08-28 10:25:55 +0300

    Лекция 2 Пример 1: вынос элемента в отдельный файл

---
+++ src/AppElement.jsx
@@ -0,0 +1,41 @@
+import 'antd/dist/antd.css';
+import './index.css';
+import {Avatar, List} from 'antd';
+
+const data = [
+    {
+        title: 'Ant Design Title 1',
+    },
+    {
+        title: 'Ant Design Title 2',
+    },
+    {
+        title: 'Ant Design Title 3',
+    },
+    {
+        title: 'Ant Design Title 4',
+    },
+];
+
+
+const appElement =
+    <div>
+        <p>
+            <h2>Hello world</h2>
+        </p>
+        <List
+            itemLayout="horizontal"
+            dataSource={data}
+            renderItem={item => (
+                <List.Item>
+                    <List.Item.Meta
+                        avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"/>}
+                        title={<a href="https://ant.design">{item.title}</a>}
+                        description="Ant Design, a design language for background applications, is refined by Ant UED Team"
+                    />
+                </List.Item>
+            )}
+        />
+    </div>
+
+export default appElement;
