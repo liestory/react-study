ecba482ce1fb482b5b0c7822f7b941720b5c7cfe
Igor Nemykin <nemykin.i.o@sberbank.ru>
2021-08-28 06:53:52 +0300

    Задание 6: Создать новое приложение React https://www.youtube.com/embed/0grybmrgc2Q с использованием Material-UI library https://www.youtube.com/embed/GMfAgHcPSqc https://www.youtube.com/embed/RVSR2p6vxi0 проект и разместить на странице адаптивное меню

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+import {AppBar, Box, Container, IconButton, Toolbar, Typography} from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu'
+import Button from '@material-ui/core/Button'
 import './App.css';
+import {makeStyles} from '@material-ui/core/styles'
+
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginRight: theme.spacing(3),
+    },
+    title: {
+        flexGrow: 1,
+    }
+}))
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const classes = useStyles();
+    return (
+        <AppBar position="fixed">
+            <Container fixed>
+                <Toolbar>
+                    <IconButton edge="start"
+                                color="inherit"
+                                aria-label="menu"
+                                className={classes.menuButton}>
+                        <MenuIcon/>
+
+                    </IconButton>
+                    <Typography variant="h5" className={classes.title}> Обучение React </Typography>
+                    <Box mr={3}>
+                        <Button color="inherit" variant="outlined"> Вход </Button>
+                    </Box>
+                    <Button color="secondary" variant="contained"> Регистрация </Button>
+                </Toolbar>
+            </Container>
+        </AppBar>
+    );
 }
 
 export default App;
