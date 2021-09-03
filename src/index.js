77dec8d28f4d573fe763fe9ec11c77b009d91bcd
Igor Nemykin <nemykin.i.o@sberbank.ru>
2021-09-04 02:12:49 +0300

    >Задание 2: Создать новое приложение React и добавьте ссылку на https://stackblitz.com/ в историю карточки. Приложение часы из примера в задании 1 с изменением раз в 30 секунд.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function clock() {
+    const element = (
+        <div>
+            <h1>Hello</h1>
+            <h2>Time is {new Date().toLocaleTimeString()}</h2>
+        </div>
+    );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    ReactDOM.render(
+        element,
+        document.getElementById('root')
+    );
+
+};
+
+
+setInterval(clock, 30000);
