444bcebdfe749b721975e57abe836139b8e2e14a
Igor Nemykin <nemykin.i.o@sberbank.ru>
2021-09-11 00:39:34 +0300

    Задание 7: Дан следующий стейт: this.state = {show: true, name: 'Иван'}; Изначально результат работы метода render следующий ('Иван' берется из this.state.name): <div><p>Привет, Иван!</p></div> Сделайте так, чтобы если this.state.show был равен true, то выводился текст 'Привет, Иван', а если равен false - то 'Пока, Иван'

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    show: true,
+    name: 'Иван'
+  }
+
+  onChangeShow = () => {
+    this.setState({show: !this.state.show})
+  }
+
+  render() {
+    return (
+        <div>
+          {this.state.show ? "Привет, " : "Пока, "}{this.state.name}
+          <button onClick={this.onChangeShow}>Change show</button>
+        </div>
+    )
+  }
 }
 
 export default App;
