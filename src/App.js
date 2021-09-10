7e77b826f3b22ea3c8b60bba4436bfae8a246a1f
Igor Nemykin <nemykin.i.o@sberbank.ru>
2021-09-11 01:01:22 +0300

    Задание 9: Дан массив с числами. Выведите эти числа каждое в своем <span> так, чтобы на выводе к каждому числу прибавлялось число 3.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      listNumbers: [1, 3, 2, 5]
+    }
+  }
+
+
+  render() {
+    return (
+        <div>
+          <h2>Список чисел с + 3</h2>
+          <div>
+            <ul>
+              {this.state.listNumbers.map((listNumber, index) => <li key={index.toString()}> {listNumber + 3}</li>)}
+            </ul>
+          </div>
+        </div>
+    )
+  }
 }
 
 export default App;
