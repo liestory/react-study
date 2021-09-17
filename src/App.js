3543644e44688bd37ccfc1ca519a6c9b05e9cfcb
Igor Nemykin <nemykin.i.o@sberbank.ru>
2021-09-17 19:35:10 +0300

    3: Посмотреть и кратко описать в карточке сюжет видео ролика. Перепишите пример с видео и добавьте ссылку в комментариях. https://www.youtube.com/embed/m_mtV4YaI8c

--- src/App.js
+++ src/App.js
@@ -1,25 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// eslint-disable-next-line no-undef
+const ErrorComponent = () => <div>{props.ignore}</div>
+
+class App extends React.Component {
+
+    constructor(props) {
+        console.log('constructor')
+        super(props);
+        this.state = {
+            counter: 0,
+            seed: 0,
+            initializing: true,
+        }
+
+        this.increment = () => {
+            this.setState({counter: ++this.state.counter})
+        }
+        this.decrement = () => {
+            this.setState({counter: --this.state.counter})
+        }
+    }
+
+
+    static getDerivedStateFromProps(props, state) {
+        if (props.seed && state.seed !== props.seed) {
+            return {
+                seed: props.seed,
+                counter: props.counter,
+            }
+        }
+        return null
+    }
+
+    componentDidMount() {
+        console.log('componentDidMount')
+        setTimeout(() => {
+            this.setState({initializing: false})
+        }, 500)
+        console.log('------------------------')
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        console.log('shouldComponentUpdate')
+        if (nextProps.ignoreProps && this.props.ignoreProps !== nextState.ignoreProps) {
+            console.log('shouldComponentUpdate - NOT RENDER')
+            return false;
+        }
+        console.log('shouldComponentUpdate - RENDER')
+        return true;
+    }
+
+    getSnapshotBeforeUpdate(prevProps, prevState) {
+        console.log('getSnapshotBeforeUpdate')
+        return null;
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log('componentDidUpdate');
+        console.log('------------------------')
+    }
+
+    componentWillUnmount() {
+        console.log('componentWillUnmount')
+        console.log('------------------------')
+    }
+
+    componentDidCatch(error, info)  {
+        console.log('componentDidCatch');
+        this.setState({error: info})
+        console.log('------------------------');
+    }
+
+    render() {
+        console.log('render')
+        if (this.state.initializing) {
+            return <div>Initializing...</div>
+        }
+
+        if (this.props.showErrorComponent && this.state.error) {
+            return <div>We have encounter an error! {this.state.error}</div>
+        }
+        return (
+            <div>
+                <button onClick={this.increment}>Increment</button>
+                <button onClick={this.decrement}>Decrement</button>
+                <div className="counter">
+                    Counter: {this.state.counter}
+                </div>
+                {this.props.showErrorComponent ? <ErrorComponent/> : null}
+            </div>
+        )
+    }
 }
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,60 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+// import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+
+class Index extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            mount: true,
+            ignoreProps: 0,
+            seed: 40,
+            showErrorComponent: false,
+        }
+
+        this.mountCounter = () => {
+            this.setState({mount: true})
+        };
+        this.unMountCounter = () => {
+            this.setState({mount: false})
+        };
+
+        this.ignoreProps = () => {
+            this.setState({ignoreProps: Math.random()})
+        }
+
+        this.seedGenerator = () => {
+            this.setState({seed: Number.parseInt(Math.random() * 100)})
+        }
+
+        this.toggleError = () => {
+            this.setState({showErrorComponent: !this.state.showErrorComponent})
+        }
+    }
+
+
+    render() {
+        return (
+            <div>
+                <button onClick={this.mountCounter} disabled={this.state.mount}>Mount Counter</button>
+                <button onClick={this.unMountCounter} disabled={!this.state.mount}>Unmount</button>
+                <button onClick={this.ignoreProps}>Ignore Props</button>
+                <button onClick={this.seedGenerator}>Generate Seed</button>
+                <button onClick={this.toggleError}>Toggle Error</button>
+                {this.state.mount ? <App
+                        ignoringProps={this.state.ignoreProps}
+                        seed={this.state.seed}
+                        showErrorComponent={this.state.showErrorComponent}/>
+                    : null}
+
+            </div>)
+    }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Index/>,
+    document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
