3ef357cb05324eb5db0b73132636c4fab6878e87
Igor Nemykin <nemykin.i.o@sberbank.ru>
2021-09-18 12:21:15 +0300

    Лекция 5 пример 4:

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
+// const level1 = ({title}) => <level2 title={title}></level2>
+// const level2 = ({title}) => <level3 title={title}></level3>
+// const level3 = ({title}) => <h2 title={title}></h2>
+// const App = () => <level1 title="level App"></level1>
+let TittleContext = React.createContext();
+
+const level3 = () => <TittleContext.Consumer> {({title}) => <h2>{title}</h2>}</TittleContext.Consumer>
+const level2 = () => <level3></level3>
+const level1 = () => <level2></level2>
+const App = () => {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
-    );
+        <TittleContext.Provider value={{title: 'level App'}}>
+            <level1></level1>
+        </TittleContext.Provider>
+    )
 }
 
 export default App;
