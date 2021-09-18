52be6b10e938f5a6e1252f8059dfeb4ec389e722
Igor Nemykin <nemykin.i.o@sberbank.ru>
2021-09-18 12:35:16 +0300

    Лекция 5 пример 5:

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
 import './App.css';
 import React from 'react';
 
-// const level1 = ({title}) => <level2 title={title}></level2>
-// const level2 = ({title}) => <level3 title={title}></level3>
-// const level3 = ({title}) => <h2 title={title}></h2>
-// const App = () => <level1 title="level App"></level1>
-let TittleContext = React.createContext();
+let TittleContext = React.createContext({title: "title"});
 
-const level3 = () => <TittleContext.Consumer> {({title}) => <h2>{title}</h2>}</TittleContext.Consumer>
-const level2 = () => <level3></level3>
-const level1 = () => <level2></level2>
-const App = () => {
-    return (
-        <TittleContext.Provider value={{title: 'level App'}}>
-            <level1></level1>
-        </TittleContext.Provider>
-    )
+
+class Level3 extends React.Component {
+    static contextTypes = TittleContext;
+
+    render() {
+        return (
+            <h2>{this.context.title}</h2>
+        )
+    }
 }
+
+const Level2 = () => <Level3/>
+const Level1 = () => <Level2/>
+
+class App extends React.Component {
+
+    render() {
+        return (
+            <TittleContext.Provider value={{title: 'level App'}}>
+                <Level1/>
+            </TittleContext.Provider>
+        )
+    }
+}
+
 
 export default App;
