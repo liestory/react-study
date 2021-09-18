c302152d97b7a6828c042c8e4f1ca179e7fed882
Igor Nemykin <nemykin.i.o@sberbank.ru>
2021-09-18 10:42:05 +0300

    Лекция 5 пример 2:

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.link = React.createRef(); //{current: null}
+//     this.state = {}
+//   }
+//
+//   render() {
+//     const nodes = this.link.current
+//     console.log(nodes)
+//     return (
+//         <div ref={this.link} className="App">
+//           <input type="number" ref={this.link}/>
+//           <button onClick={() => this.link.current.focus()}>Get Focus Ref</button>
+//         </div>
+//     )
+//   }
+// }
+
 function App() {
+    const link = React.useRef(null);
+
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+            <input type="number" ref={link}/>
+            <button onClick={() => link.current.focus()}>Get Focus Ref</button>
         </div>
-    );
+    )
 }
 
 export default App;
