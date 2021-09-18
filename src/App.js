562ad3fa90647f74a5abe6d513a51fd7ae6ce1cb
Igor Nemykin <nemykin.i.o@sberbank.ru>
2021-09-18 13:47:24 +0300

    Лекция 5 пример 6:

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import React from 'react';
+import {useForm} from 'react-hook-form'
 import './App.css';
 
 function App() {
+    const {
+        register,
+        formState: {errors, submitCount},
+        handleSubmit
+    } = useForm();
+
+    const onSubmit = (date) => console.log(date)
+
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
-    );
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <h2>Форма запросов {submitCount}</h2>
+            <input {...register("firstName")}/><br/>
+            <input {...register("lastName", {required: true})} type="text"/><br/>
+
+
+            <input {...register("age", {
+                required: true,
+                max: 35, min: 18,
+                pattern: /\d{2}/
+            })}/><br/>
+            <input type="submit"/><br/>
+            {errors.lastName && <span>LastName is required.</span>}<br/>
+            {errors.age && <span>Age is wrong.</span>}<br/>
+
+        </form>
+    )
 }
+
 
 export default App;
