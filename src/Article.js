bb31e8d9dad5f3daf85f233380139e15eb5f8003
Igor Nemykin <nemykin.i.o@sberbank.ru>
2021-09-24 21:29:09 +0300

    2: Посмотреть и кратко описать в карточке сюжет видео ролика. Перепишите пример с видео и добавьте ссылку в комментариях. https://www.youtube.com/embed/qdaDgC_xU0w

---
+++ src/Article.js
@@ -0,0 +1,63 @@
+import React from 'react';
+
+class Article extends React.PureComponent {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            isOpen: props.defaultOpen,
+            count: 0
+        }
+    }
+
+
+    componentWillMount() {
+        console.log('---', 'componentWillMount')
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.defaultOpen !== this.props.defaultOpen) {
+            this.setState({
+                isOpen: nextProps.defaultOpen
+            })
+        }
+    }
+
+    componentWillUpdate() {
+        console.log('---', 'componentWillUpdate')
+    }
+
+    incrementCounter = () => {
+        this.setState({
+            count: this.state.count + 1
+        })
+    }
+
+    render() {
+        const {article, isOpen} = this.props;
+        const style = {width: '50%'}
+        const body = isOpen && <section className='card-text'>{article.text}</section> < /section>
+        return (
+            <div className="card mx-auto" style={style}>
+                <div className="card-header">
+                    <h2 onClick={this.incrementCounter}>
+                        {article.title}
+                        clicked {this.state.count}
+                        <button onClick={this.handleClick}
+                                className="btn btn-primary btn-lg">
+                            {isOpen ? 'closed' : 'open'}
+                        </button>
+                    </h2>
+                </div>
+                <div className="card-body">
+                    <h6 className="card-subtitle text-muted">
+                        creation date: {(new Date(article.date).toDateString())}
+                    </h6>
+                    {body}
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Article;
---
+++ src/ArticleList.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Article from './Article';
+
+class ArticleList extends React.PureComponent {
+
+    state = {
+        openArticleId: null,
+    }
+
+    handleClick = openArticleId => this.setState({
+        openArticleId: this.state.openArticleId === openArticleId ? null : openArticleId
+    })
+
+    render() {
+        console.log('---', 2)
+        const articleElements = this.props.articles.map((article, index) => {
+            <li key={article.id}
+                className="article-list_li">
+                <Article article={article}
+                         isOpen={this.state.openArticleId === article.id}
+                         onButtonClick={this.handleClick.bind(this, article.id)}/>
+            </li>
+        })
+        return (<ul>
+            {articleElements}
+        </ul>)
+    }
+}
+
+export default ArticleList;
