20ca8a436cbe5b3c4a10178748ba9a225051d0f8
Igor Nemykin <nemykin.i.o@sberbank.ru>
2021-09-25 11:52:52 +0300

    лекция 6 пример 2:

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import React, {useLayoutEffect, useState} from 'react';
+import axios from "axios";
 import './App.css';
 
 function App() {
+    const [data, setData] = useState([]);
+
+    useLayoutEffect(() => {
+        axios.get("https://jsonplaceholder.typicode.com/posts")
+            .then((response) => {
+                console.log()
+                setData(response.data)
+            })
+    }, []);
+
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+            <ul>
+                {data.map((item, idx) => <li key={idx}>{item.title}</li>)}
+            </ul>
         </div>
     );
 }
