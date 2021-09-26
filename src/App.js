08d28e8196a5d69bf879765074772b1d035a5e13
Igor Nemykin <nemykin.i.o@sberbank.ru>
2021-09-26 10:42:17 +0300

    лекция 6 пример 7: primereact

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { InputText } from "primereact/inputtext";
+import { InputNumber } from "primereact/inputnumber";
+import { InputSwitch } from "primereact/inputswitch";
+import { Card } from "primereact/card";
+import React from "react";
 
 function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
-    );
+  const [value, setValue] = React.useState("");
+  const [valueNumber, setValueNmber] = React.useState(0);
+  const [valueSwitch, setvalueSwitch] = React.useState("");
+
+  return (
+    <Card
+      footer={
+        <span>
+          <input type="submit" />
+        </span>
+      }
+    >
+      <from>
+        <span className="p-float-label">
+          <InputText
+            id="in"
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+          />
+          <label htmlFor="in">First Name</label>
+        </span>
+        <span className="p-float-label">
+          <InputNumber
+            value={valueNumber}
+            onValueChange={(e) => setValueNmber(e.value)}
+            mode="decimal"
+          />
+          {/* <label>Age</label> */}
+        </span>
+        <span>
+          <InputSwitch
+            checked={valueSwitch}
+            onChange={(e) => setvalueSwitch(e.value)}
+          />
+        </span>
+      </from>
+    </Card>
+  );
 }
 
 export default App;
