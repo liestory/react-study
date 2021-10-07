0e58575fba8d8fdba8b26da18b97f9b2715defbb
Igor Nemykin <nemykin.i.o@sberbank.ru>
2021-10-07 18:55:07 +0300

    >10: Написать новое приложение и добавить код с картинкой и ссылкой на приложение из stackblitz.com. Требования к форме. Поля ввода: Фамилия, Имя, Отчество, Дата рождения, Телефон, E-mail. Добавить кнопку "Отправить". Функционал кнопки отправка данных по адресу POST: https://jsonplaceholder.typicode.com/posts Требования к запросу: по нажатию "Отправить" заблокировать изменение полей, после ответа с сервера поменять все поля ввода на текстовые поля. Валидация полей: Фамилия - "Не более 50 символа",Имя - "Не более 31 символа", Отчество - "Не более 31 символа", Дата рождения - в формате "dd.MM.yyyy", Телефон-"Маска “+7(NNN) NNN NN NN”, где N – любая цифра", E-mail - "валидация email". Все поля обязательны, кроме E-mail

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
+import React, {useEffect} from 'react';
+import {useForm} from 'react-hook-form'
 import './App.css';
 
+
 function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
+    const {
+        register,
+        formState: {errors, submitCount},
+        handleSubmit
+    } = useForm();
+
+    useEffect(() => {
+        const request = {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify([{register: register}])
+        };
+        fetch('https://reqres.in/api/posts', request)
+            .then(response => response.json())
+    }, []
     );
+
+
+    const onSubmit = (date) => console.log(date)
+
+    return (
+        <form onSubmit={handleSubmit(onSubmit)}>
+            {submitCount === 0 || errors !== null ? (
+                <div>
+                    <h2>Форма запросов {submitCount}</h2>
+                    <label>Фамилия </label>
+                    <input {...register("lastName", {required: true, maxLength: 50})}
+                           type="text"/><br/>
+                    <label>Имя </label>
+                    <input {...register("firstName", {required: true, maxLength: 31})}
+                           type="text"/><br/>
+                    <label>Отчество </label>
+                    <input {...register("secondName", {required: true, maxLength: 31})}
+                           type="text"/><br/>
+                    <label>Дата рождения </label>
+                    <input {...register("birthDate", {
+                        required: true,
+                        pattern: /\d{2}.\d{2}.\d{4}/
+                    })} type="text"/><br/>
+                    <label>Телефон </label>
+                    <input {...register("phone", {
+                        required: true, pattern: /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/
+                    })} type="text"/><br/>
+                    <label>E-mail </label>
+                    <input {...register("email", {
+                        pattern: /^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$/
+                    })} type="text"/><br/>
+                    <input type="submit"/><br/>
+                    {errors.lastName && <span>LastName is required.</span>}<br/>
+                    {errors.age && <span>Age is wrong.</span>}<br/>
+                </div>
+            ) : (
+                <div>
+                    <h2>Форма запросов {submitCount}</h2>
+                    <label>Фамилия </label>
+                    <p>{register.lastName}</p><br/>
+                    <label>Имя </label>
+                    <p>{register.firstName}</p><br/>
+                    <label>Отчество </label>
+                    <p>{register.secondName}</p><br/>
+                    <label>Дата рождения </label>
+                    <p>{register.birthDate}</p><br/>
+                    <label>Телефон </label>
+                    <p>{register.phone}</p><br/>
+
+                    {register.email !== null && <div><label>E-mail </label><p>{register.email}</p><br/></div>}
+
+                </div>
+            )}
+        </form>
+
+    )
 }
 
 export default App;
