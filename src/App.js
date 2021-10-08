a7e7479ac58e41b6fa6ba2cfbe879d13667d991b
Igor Nemykin <nemykin.i.o@sberbank.ru>
2021-10-08 13:49:01 +0300

    8: Посмотреть и кратко описать в карточке сюжет видео ролика. Перепишите пример с видео и добавьте ссылку в комментариях. https://www.youtube.com/embed/4xjWX3oRQoc

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native'
 
-function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
-    );
+class App extends React.Component {
+
+
+    constructor(props) {
+        super(props);
+
+        console.log("constructor")
+    }
+
+
+    static getDerivedStateFromProps() {
+        console.log("getDerivedStateFromProps")
+    }
+
+    componentDidMount() {
+        console.log("componentDidMount")
+    }
+
+    render() {
+        console.log("render");
+        return (
+            <View style={styles.viewStyle}>
+                <Text style={styles.textStyle}>StarGate </Text>
+            </View>
+        )
+    }
 }
+
+const styles = StyleSheet.create({
+    viewStyle: {
+        backgroundColor: '#30d0fe',
+        height: 116,
+        justifyContent: 'center',
+        paddingLeft: 22,
+        paddingTop: 71,
+        shadowColor: '#000',
+        shadowOffset: {width: 0, height: 2},
+        shadowOpacity: 0.2,
+        elevation: 2,
+        position: 'relative'
+    }
+})
 
 export default App;
