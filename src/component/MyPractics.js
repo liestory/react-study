cf63954443f2d7732d6600f5ea0230ea2f627ced
Igor Nemykin <nemykin.i.o@sberbank.ru>
2021-10-09 09:43:08 +0300

    1: Добавить в свой итоговый проект меню "Мои проекты на курсе". Перечислить в рамках страницы "Мои проекты на курсе" все темы уроков и в каждом уроке добавить список всех домашних заданий и ссылки на проделанные задачи.

---
+++ src/component/MyPractics.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class MyPractics extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default MyPractics;
