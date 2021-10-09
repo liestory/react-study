88108f3b5f1fc21015dc96f87ef58930939a9764
Igor Nemykin <nemykin.i.o@sberbank.ru>
2021-10-09 15:21:14 +0300

    1: Добавить в свой итоговый проект меню "Мои проекты на курсе". Перечислить в рамках страницы "Мои проекты на курсе" все темы уроков и в каждом уроке добавить список всех домашних заданий и ссылки на проделанные задачи.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import React from 'react';
-import MyPractics from "./component/MyPractics";
+import Menu from "./component/Menu";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
     return (
         <div className="App">
-            <MyPractics/>
+            <header style={{'background-color': '#282c34'}}>
+                <h1>Визитка Игоря</h1>
+            </header>
+            <Menu/>
         </div>
     );
 }
--- src/component/MyPractics.js
+++ src/component/MyPractics.js
@@ -1,15 +1,41 @@
 import React from 'react';
+import ListGroup from 'react-bootstrap/ListGroup'
+import practics from "../jsons/practics";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './MyPractics.css'
+import {BrowserRouter, Link, Route, Switch, useRouteMatch} from 'react-router-dom'
+import Lesson from "./Lesson";
 
-class MyPractics extends React.Component {
-    constructor(props) {
-        super(props);
-    }
 
-    render() {
+const MyPractics = (props) => {
+    let match = useRouteMatch()
+    {
+
         return (
-            <div>
-
-            </div>
+            <BrowserRouter>
+                <div>
+                    <ListGroup as="ul" bg="dark" variant="dark" className="links">
+                        {practics.map((practic) => (
+                            <ListGroup.Item key={practic.id} variant="dark">
+                                <Link to={`${match.url}/${practic.data.card.idShort}`}>
+                                    Урок: {practic.data.card.name}
+                                </Link>
+                            </ListGroup.Item>
+                        ))}
+                    </ListGroup>
+                    <Switch>
+                        {practics.map((practic) => (
+                            <Route key={practic.id} path={`${match.path}/${practic.data.card.idShort}`}>
+                                <Lesson
+                                    name={practic.data.card.name}
+                                    text={practic.data.text}
+                                    className="lesson"
+                                />
+                            </Route>
+                        ))}
+                    </Switch>
+                </div>
+            </BrowserRouter>
         )
     }
 }
