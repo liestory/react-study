a8af7dfb9e204b201bf937237eea24094edd79bf
Igor Nemykin <nemykin.i.o@sberbank.ru>
2021-10-12 13:04:40 +0300

    странные пробы пера, но вроде в нужном направлении

--- src/component/modals/WaitForCloseModal.js
+++ src/component/modals/WaitForCloseModal.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import waitLogo from 'image_861503170058263518871.gif';
 
 function WaitForCloseModal(props) {
     return (
         <div>
-            <img src={waitLogo}/>
+            <img src="https://99px.ru/sstorage/86/2017/03/image_861503170058263518871.gif"/>
         </div>
 
     )
