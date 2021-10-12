f4410758565dedb06c92cba4e02ae0996b905d51
Igor Nemykin <nemykin.i.o@sberbank.ru>
2021-10-12 12:07:18 +0300

    странные пробы пера, но вроде в нужном направлении

--- src/App.js
+++ src/App.js
@@ -5,14 +5,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import MyPractics from "./component/MyPractics";
 import Issues from "./component/Issues";
 import {BrowserRouter, Route, Switch,} from "react-router-dom";
-// import
+
 
 function App() {
     return (
         <div className="App">
             <BrowserRouter>
                 <header style={{'background-color': '#282c34'}}>
-                    <h1>/>---------------------------/></h1>
+                    <h1>---------------------------</h1>
                     <Menu/>
                 </header>
 
